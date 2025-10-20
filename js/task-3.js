91c15081fe132ae88d98df1585f7d0709ae3d58e
Yurii <yuranhik001@gmail.com>
2025-10-20 20:19:52 +0300

    init

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,12 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    const totalWhidth = contentWidth + (paddingWidth + borderWidth) * 2;
-    return totalWhidth;
+function checkForSpam(message) {
+    const normalizeMessage = message.toLowerCase();
+    return (normalizeMessage.includes('spam')) || (normalizeMessage.includes('sale'));
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px")); 
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
